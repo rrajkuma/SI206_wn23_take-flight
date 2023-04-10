09646e9f9eae18b9f83c509d5b0431fc541de65e
Rachel <rrajkuma@umich.edu>
2023-04-10 19:39:20 -0400

    added main function for testing

--- flight.py
+++ flight.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import requests
 import sqlite3
 import json
@@ -11,9 +11,20 @@ aerobox_params = {
 
 aviation_params = {'access_key': '4916bf37e9a67451871a02a2b8e26113'}
 
+#variables to store the urls for the APIs
+#this url will answer 'How many daily flights to different destinations from an airport?'
+#and "What are the most popular routes from an airport?"
+#NOTE: for each airpot of interest, change the IATA code (currently DTW), and rerun the function.
+aero_stat_url = "https://aerodatabox.p.rapidapi.com/airports/iata/DTW/stats/routes/daily"
+
 #grabs information using a url and one of the API keys above
 def get_API_info(url, params):
     response = requests.get(url, params)
     data = response.text
     res_dict = json.loads(data)
     return res_dict
+
+def main():
+    get_API_info(aero_stat_url, aerobox_params)
+
+main()
